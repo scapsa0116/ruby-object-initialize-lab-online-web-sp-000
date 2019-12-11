cbc705a2dc025c08978eec1b9571cb058d63a7f5
Stela Capsa <stela.capsa@gmail.com>
2019-12-11 15:15:14 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,62 @@
+class Dog
+  attr_accessor :name, :breed
+ 
+   def initialize(breed = "Mutt" )
+    @breed = breed
+     name
+  end
+  
+ 
+  
+ fido = Dog.new(name) 
+
+  
+  
+  pug = Dog.new(name)
+
+  
+end
+  
+  
+  
+  
+  
+  
+  
+  
+#   def initialize(name)
+#   @name = name
+#   end
+ 
+#   def name=(name)
+#     @name = name
+#   end
+ 
+#   def name
+#     @name
+#   end
+#   fido = Dog.new
+#   fido.name = ("Fido" "Pug")
+#   puts fido.name 
+  
+#   # pug = Dog.new
+#   # pug.name = ("Pug")
+#   # puts pug.name
+# end
+  
+  
+  
+# class Dog
+#   def breed=(breed)
+#     @breed = breed
+#   end
+ 
+#   def breed
+#     @breed
+#   end
+
+#   mutt = Dog.new
+#   mutt.breed = ("Mutt")
+#   puts mutt.breed
+# end
+
