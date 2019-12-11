2c657338b1aed9ac087413bcc416c7e5f6e8901e
Stela Capsa <stela.capsa@gmail.com>
2019-12-11 15:47:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,8 @@
 class Dog
- 
-   def initialize(name)
-    @name = name
-  end
- 
-  def name=(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
-    
-  def initialize(breed = "Mutt")
-    @breed = breed
-  end
- 
-  def breed=(breed)
-    @name = name
-  end
- 
-  def breed
-    @breed
-  end
+ def initialize(name, breed)
+  
+end
+  
   Dog.new("Fido", "Pug")
  
   
