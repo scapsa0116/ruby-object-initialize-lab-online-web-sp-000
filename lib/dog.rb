7adf912499cc257f727e2cac6edb59d798fbeacb
Stela Capsa <stela.capsa@gmail.com>
2019-12-11 16:06:23 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+ 
+ def initialize(name, breed = "Mutt")
+  @name = name
+  @breed = breed
+end
+  
+ 
+ 
+  
+Dog.new("Fido")
+
+  
+  
+Dog.new("Pug")
+
+end
+  
+  
+  
+  
+  
+  
+  
+  
