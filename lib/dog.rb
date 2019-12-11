4605490689a6b601e31c556e8545481904302f52
Stela Capsa <stela.capsa@gmail.com>
2019-12-11 15:24:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,18 @@
 class Dog
  
    def initialize(name)
-   
-    
+    @name = name
   end
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+    
+  
   
  
   
@@ -22,39 +31,3 @@ end
   
   
   
-#   def initialize(name)
-#   @name = name
-#   end
- 
-#   def name=(name)
-#     @name = name
-#   end
- 
-#   def name
-#     @name
-#   end
-#   fido = Dog.new
-#   fido.name = ("Fido" "Pug")
-#   puts fido.name 
-  
-#   # pug = Dog.new
-#   # pug.name = ("Pug")
-#   # puts pug.name
-# end
-  
-  
-  
-# class Dog
-#   def breed=(breed)
-#     @breed = breed
-#   end
- 
-#   def breed
-#     @breed
-#   end
-
-#   mutt = Dog.new
-#   mutt.breed = ("Mutt")
-#   puts mutt.breed
-# end
-
